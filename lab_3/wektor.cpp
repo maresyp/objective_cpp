c5b1250eebcb3d26aa4b3d34c11ff7fb6e6b9508
maresyp <mattq.2000@icloud.com>
2021-11-05 19:42:38 +0100

    rewrite

--- lab_3/wektor.cpp
+++ lab_3/wektor.cpp
@@ -1,27 +1,14 @@
 
 #include <iostream>
 
-class Point {
-   private:
-    double x;
-    double y;
-
-   public:
-    double get_x() {
-        return this->x;
-    }
-    double get_y() {
-        return this->y;
-    }
-};
 class Wektor {
-   private:
+   public:
     static unsigned int obj_count;
-    const Point* start;
-    const Point* end;
+    double start;
+    double end;
 
    public:
-    Wektor(const Point* start, const Point* end) {
+    Wektor(double start, double end) {
         this->start = start;
         this->end = end;
         obj_count++;
@@ -30,13 +17,17 @@ class Wektor {
         return obj_count;
     }
     Wektor* add(const Wektor& vec) {
-        //
+        return new Wektor((this->start + vec.start), (this->end + vec.end));
     }
+
     Wektor* sub(const Wektor& vec) {
-        //
+        return new Wektor((this->start - vec.start), (this->end - vec.end));
     }
-    Wektor* mul(const Wektor& vec) {
-        //
+    double mul(const Wektor& vec) {
+        return (this->start * vec.start) + (this->end * vec.end);
+    }
+    void display() {
+        std::cout << "[" << this->start << "," << this->end << "] ";
     }
     ~Wektor() {
         std::cout << "Zadanie wykonane" << std::endl;
@@ -45,6 +36,19 @@ class Wektor {
 
 unsigned int Wektor::obj_count = 0;
 int main() {
+    Wektor one = Wektor(3, -1);
+    Wektor two = Wektor(2, 2);
+
+    Wektor* result = one.add(two);
+    result->display();
+
+    result = one.sub(two);
+    result->display();
+
+    std::cout << one.mul(two) << " ";
+
     std::cout << Wektor::get_obj_count() << std::endl;
+
+    delete result;
     return 0;
 }
