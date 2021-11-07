86c3aa286c340138098a44d534e4c6e23d35ec9b
maresyp <mattq.2000@icloud.com>
2021-11-07 08:27:24 +0100

    init

---
+++ lab_4/worker.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+class Pracownik {
+    std::string imie;
+    std::string imie;
+    std::string imie;
+    std::string imie;
+    double pensja;
+    std::string dzial;
+};
