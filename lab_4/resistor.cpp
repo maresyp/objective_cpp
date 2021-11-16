f5d237ac69b27e081e3c6d2212b0067e4d220149
maresyp <mattq.2000@icloud.com>
2021-11-16 13:06:00 +0100

    refactor constructors

--- lab_4/resistor.cpp
+++ lab_4/resistor.cpp
@@ -2,35 +2,40 @@
 #include <iostream>
 
 class Resistor {
-   private:
+private:
     double resistance;
 
-   public:
-    Resistor() {
-        this->resistance = 0;
+public:
+    Resistor() : resistance(0) {
     }
-    Resistor(double resistance) {
-        this->resistance = resistance;
+
+    explicit Resistor(double resistance) : resistance(resistance) {
     }
-    double r() {
+
+    double r() const {
         return this->resistance;
     }
-    void r(double resistance) {
-        this->resistance = resistance;
+
+    void r(double res) {
+        this->resistance = res;
     }
+
     friend Resistor operator+(const Resistor &res1, const Resistor &res2);
+
     friend Resistor operator*(const Resistor &res1, const Resistor &res2);
+
     friend std::ostream &operator<<(std::ostream &output, const Resistor &Res);
 };
 
 Resistor operator+(const Resistor &res1, const Resistor &res2) {
     return Resistor(res1.resistance + res2.resistance);
 }
+
 Resistor operator*(const Resistor &res1, const Resistor &res2) {
     return Resistor((res1.resistance * res2.resistance) / (res1.resistance + res2.resistance));
 }
-std::ostream &
-operator<<(std::ostream &output, const Resistor &Res) {
+
+std::ostream &operator<<(std::ostream &output, const Resistor &Res) {
     output << "R: " << Res.resistance;
     return output;
 }
