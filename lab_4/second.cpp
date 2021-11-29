1cc7e5aa297b6f056561a9d39faec5eec1a0d726
maresyp <mattq.2000@icloud.com>
2021-11-29 15:08:41 +0100

    impl classes

--- lab_4/second.cpp
+++ lab_4/second.cpp
@@ -1,4 +1,48 @@
-//
-// Created by Mateusz Pysera on 21/11/2021.
-//
 
+#include <iostream>
+
+class Person {
+protected:
+    std::string name;
+    std::string last_name;
+    std::string date_of_birth;
+
+public:
+    virtual void print_info() {
+        std::cout << name << " " << last_name << " " << date_of_birth << " ";
+    }
+};
+
+class Student : public Person {
+protected:
+    std::string year;
+    uint16_t group;
+    std::string index;
+
+public:
+    virtual void print_info() {
+        Person::print_info();
+        std::cout << year << " " << group << " " << index << " ";
+    }
+};
+
+class FootballPlayer : public Person {
+protected:
+    std::string position;
+    std::string club;
+    uint32_t score_count;
+
+public:
+    virtual void print_info() {
+        Person::print_info();
+        std::cout << position << " " << club << " " << score_count << " ";
+    }
+
+    void add_score() {
+        score_count++;
+    }
+};
+
+int main() {
+    return 0;
+}
