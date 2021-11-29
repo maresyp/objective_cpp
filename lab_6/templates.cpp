bbec495d6744af919feaf08bb2eab48b55083245
maresyp <mattq.2000@icloud.com>
2021-11-29 15:08:22 +0100

    impl Sequence class

--- lab_6/templates.cpp
+++ lab_6/templates.cpp
@@ -1,4 +1,29 @@
-//
-// Created by Mateusz Pysera on 27/11/2021.
-//
 
+#include <iostream>
+
+template <typename T>
+class Sequence {
+   public:
+    Sequence<T>();
+    T get_nth(T n);
+    bool in_sequence(T elem);
+};
+template <typename T>
+Sequence<T>::Sequence() {}
+
+template <typename T>
+T Sequence<T>::get_nth(T n) {
+    if (n <= 1) return n;
+    return get_nth(n - 1) + get_nth(n - 2);
+}
+
+template <typename T>
+bool Sequence<T>::in_sequence(T elem) {
+    // TODO: impl this
+    return false;
+}
+
+int main() {
+    auto seq = Sequence<int>();
+    return 0;
+}
