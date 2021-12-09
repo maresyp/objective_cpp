8b82b40a15ce6d2e5b34b3c5ad2bab77a4065f40
maresyp <mattq.2000@icloud.com>
2021-12-09 15:52:58 +0100

    add class scratch

--- lab_6/list.cpp
+++ lab_6/list.cpp
@@ -1,4 +1,45 @@
 //
 // Created by Mateusz Pysera on 07/12/2021.
 //
+#include <iostream>
+
+template<typename T>
+class List {
+private:
+    T *data;
+    size_t capacity;
+    size_t size;
+public:
+    void sort();
+
+    T min() const;
+
+    T max() const;
+
+    size_t index() const;
+
+    void push(T value);
+
+
+};
+
+template<typename T>
+void List<T>::sort() {
+    // todo: impl sort
+}
+
+template<typename T>
+T List<T>::min() const {
+    // todo: impl min
+}
+
+template<typename T>
+T List<T>::max() const {
+    // todo: impl max
+}
+
+template<typename T>
+size_t List<T>::index() const {
+    // todo: impl index
+}
 
