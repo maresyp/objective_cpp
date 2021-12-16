42d29285d927dfc741e2cd97674598513fb2d97e
maresyp <mattq.2000@icloud.com>
2021-12-16 18:21:24 +0100

    figures.cpp

--- lab_5/figures.cpp
+++ lab_5/figures.cpp
@@ -1,75 +1,91 @@
 //
 // Created by Mateusz Pysera on 07/12/2021.
 //
+#include <iostream>
+#include <cmath>
 
 class Bryla {
 public:
-    virtual int pole() {
+    virtual double pole() {
         return 0;
     }
 
-    virtual int objetosc() {
+    virtual double objetosc() {
         return 0;
     }
 
-    virtual int przekotna() {
+    virtual double przekotna() {
         return 0;
     }
 
+    virtual ~Bryla() = default;
 };
 
 class Kula : public Bryla {
+private:
+    double r;
 public:
-    int pole() override {
-        return 0;
+    explicit Kula(double r) : r(r) {}
+
+    double pole() override {
+        return 4 * 3.14 * pow(r, 2);
     }
 
-    int objetosc() override {
-        return 0;
+    double objetosc() override {
+        return (4.0 / 3.0) * 3.14 * pow(r, 3);
     }
-
-protected:
-    ~Kula() = default;
 
 };
 
 class Prostopadloscian : public Bryla {
+private:
+    double a, b, c;
 public:
-    int pole() override {
-        return 0;
+    Prostopadloscian(double a, double b, double c) : a(a), b(b), c(c) {}
+
+    double pole() override {
+        return 2 * ((a * b) + (a * c) + (b * c));
     }
 
-    int objetosc() override {
-        return 0;
+    double objetosc() override {
+        return a * b * c;
     }
 
-    int przekotna() override {
-        return 0;
+    double przekotna() override {
+        return sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
     }
 
-protected:
-    ~Prostopadloscian() = default;
 };
 
 class Ostroslup : public Bryla {
 public:
-    int pole() override {
-        return 0;
+    double a, h;
+
+    Ostroslup(double a, double h) : a(a), h(h) {}
+
+    double pole() override {
+        return pow(a, 2) + (2 * a * h);
     }
 
-    int objetosc() override {
-        return 0;
+    double objetosc() override {
+        return (1.0 / 3.0) * pow(a, 2) * h;
     }
 
-    int przekotna() override {
-        return 0;
-    }
-
-protected:
-    ~Ostroslup() = default;
 };
 
 int main(int argc, char *argv[], char *envp[]) {
 
+    auto kula = Kula(10);
+    std::cout << "Kula objetosc: " << kula.objetosc() << "\n";
+    std::cout << "Kula pole: " << kula.pole() << "\n";
+
+    auto prostopadloscian = Prostopadloscian(5, 10, 15);
+    std::cout << "Prostopadloscian pole: " << prostopadloscian.pole() << "\n";
+    std::cout << "Prostopadloscian objetosc: " << prostopadloscian.objetosc() << "\n";
+    std::cout << "Prostopadloscian przekotna: " << prostopadloscian.przekotna() << '\n';
+
+    auto ostroslup = Ostroslup(5, 10);
+    std::cout << "Ostroslup pole: " << ostroslup.pole() << '\n';
+    std::cout << "Ostroslup objetosc: " << ostroslup.objetosc() << '\n';
     return 0;
 }
