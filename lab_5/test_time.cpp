7572b4baa6f5bdbe4bf7604d56a5e96df9abfd65
maresyp <mattq.2000@icloud.com>
2021-12-29 18:57:58 +0100

    class methods definitions

--- lab_5/test_time.cpp
+++ lab_5/test_time.cpp
@@ -2,3 +2,29 @@
 // Created by Mateusz Pysera on 07/12/2021.
 //
 
+class TestTime {
+public:
+    virtual int read_file_vr();
+
+    int read_file_non_vr();
+};
+
+int TestTime::read_file_vr() {
+    // TODO : open file and write content to variable
+    return 0;
+}
+
+int TestTime::read_file_non_vr() {
+    // TODO : open file and write content to variable
+    return 0;
+}
+
+class ChildTestTime : public TestTime {
+
+};
+
+int main() {
+    ChildTestTime cht = ChildTestTime();
+    return 0;
+}
+
