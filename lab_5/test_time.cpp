7dac6ce2538fa00da9c09350d687e0121bcaa85d
maresyp <mattq.2000@icloud.com>
2022-01-02 17:09:21 +0100

    impl

--- lab_5/test_time.cpp
+++ lab_5/test_time.cpp
@@ -1,22 +1,32 @@
 //
 // Created by Mateusz Pysera on 07/12/2021.
 //
+#include <iostream>
+#include <fstream>
+#include <chrono>
 
 class TestTime {
 public:
-    virtual int read_file_vr();
+    virtual void read_file_vr(const std::string &filename);
 
-    int read_file_non_vr();
+    void read_file_non_vr(const std::string &filename);
+
 };
 
-int TestTime::read_file_vr() {
-    // TODO : open file and write content to variable
-    return 0;
+void TestTime::read_file_vr(const std::string &filename) {
+    std::string data;
+    std::ifstream file;
+    file.open(filename);
+    file >> data;
+    file.close();
 }
 
-int TestTime::read_file_non_vr() {
-    // TODO : open file and write content to variable
-    return 0;
+void TestTime::read_file_non_vr(const std::string &filename) {
+    std::string data;
+    std::ifstream file;
+    file.open(filename);
+    file >> data;
+    file.close();
 }
 
 class ChildTestTime : public TestTime {
@@ -24,7 +34,24 @@ class ChildTestTime : public TestTime {
 };
 
 int main() {
-    ChildTestTime cht = ChildTestTime();
+    auto cht = ChildTestTime();
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i) {
+        cht.read_file_vr("data.txt");
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Virtual - 1000 x call " << duration.count() << "\n";
+
+
+    auto start2 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i) {
+        cht.read_file_non_vr("data.txt");
+    }
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);
+    std::cout << "Non Virtual - 1000 x call " << duration2.count() << "\n";
     return 0;
 }
 
