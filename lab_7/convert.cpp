0bd0e621a8f1f6c3305a19f9d25e8d64cdd02aa4
maresyp <mattq.2000@icloud.com>
2022-01-11 18:08:28 +0100

    impl convert

--- lab_7/convert.cpp
+++ lab_7/convert.cpp
@@ -2,3 +2,34 @@
 // Created by Mateusz Pysera on 02/01/2022.
 //
 
+#include <string>
+#include <iostream>
+
+int convert(std::string str) {
+    int sign = 1;
+    int base = 0;
+    int carrot = 0;
+
+    while (isspace(str[carrot])) carrot++;
+    if (str[carrot] == '-' || str[carrot] == '+') {
+        sign = (str[carrot] == '-') ? -1 : 1;
+        carrot++;
+    }
+    while (carrot < str.length()) {
+        if (str[carrot] < '0' || str[carrot] > '9')
+            throw "bad argument";
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[carrot] - '0' > 7))
+            throw "int overflow";
+        base = base * 10 + (str[carrot] - '0');
+        carrot++;
+    }
+    return base * sign;
+}
+
+int main() {
+//    std::string txt;
+//    std::cout << "Wprowadz liczbe ";
+//    std::cin >> txt;
+    std::cout << convert("1234512345678901234567890123456789");
+    return 0;
+}
